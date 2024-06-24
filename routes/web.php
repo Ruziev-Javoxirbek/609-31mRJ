fbd6ab021a4c0594e2e817990ed1342cbcc2738d
Ruziev-Javoxirbek <ruziev_zhz@edu.surgu.ru>
2024-06-24 21:29:50 +0500

    Start project

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('hello', ['title' => 'Hello 609-31mRJ SURGU!']);
+});
